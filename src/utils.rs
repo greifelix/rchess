15436324d3ef86e0dae3e73ddee868a515084dca
Felix Greiwe <greifelix@icloud.com>
2025-09-07 10:45:14 +0200

    Remove modulo operator

--- src/utils.rs
+++ src/utils.rs
@@ -12,8 +12,8 @@ pub fn idx_to_coordinates(row: usize, col: usize) -> (f32, f32) {
     let square_size = 0.05;
     let offset = 0.025;
 
-    let row_offset: f32 = offset + 3.0 * square_size - ((row % 8) as f32) * square_size;
-    let col_offset: f32 = -offset - 3.0 * square_size + ((col % 8) as f32) * square_size;
+    let row_offset: f32 = offset + 3.0 * square_size - row as f32 * square_size;
+    let col_offset: f32 = -offset - 3.0 * square_size + col as f32 * square_size;
     (row_offset, col_offset)
 }
 
